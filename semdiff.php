0bca4b0b5e3409f5b1013b1d2d03fcb697ed17b4
Lars Pelz <lars@pelz-familie.de>
2021-02-09 08:20:08 +0100

    semdiff works

--- semdiff.php
+++ semdiff.php
@@ -1,13 +1,7 @@
 <?php
 
-class Container {
-	public $snr;
-	public $fk;
-	public $name;
-	public $vorname;
-}
-
-	include 'dbconnect.inc.php';
+	include 'dbinterface.inc.php';
+	DB::connect();
 	
 	include 'auth.inc.php';
 	
@@ -23,42 +17,18 @@ class Container {
 	$sql='SELECT s.snr,s.name,s.vorname,t.fachkurz FROM ('.
 		'SELECT snr,fachkurz,COUNT(fachkurz) AS cnt FROM '.$tpref.'waehlt '.
 		'WHERE sem IN ('.$semno[0].','.$semno[1].') GROUP BY snr,fachkurz) AS t, '.$tpref.'schueler s WHERE cnt<2 and s.snr=t.snr ORDER BY snr';
-	//echo $sql;
-	$res=mysql_query($sql);
-	$base=array();
-	while ($data=mysql_fetch_assoc($res)) {
-		$c=new Container();
-		$c->snr=$data['snr'];
-		$c->fk=$data['fachkurz'];
-		$c->name=$data['name'];
-		$c->vorname=$data['vorname'];
-		$base[]=$c;
-	}
 	
-	$sem=array();
-	foreach ($base as $c) {
-		$snr=$c->snr;
-		$fkurz=$c->fk;
-		$name=$c->name;
-		$vorname=$c->vorname;
-		$res=mysql_query("SELECT sem FROM ".$tpref."waehlt WHERE sem IN (".$semno[0].",".$semno[1].") AND snr=$snr AND fachkurz='$fkurz' ORDER BY sem");
-		$data=mysql_fetch_assoc($res);
+	$persons=DB::get_assoc($sql);
+	
+	foreach ($persons as $p) {
+		$snr=$p['snr'];
+		$fkurz=$p['fachkurz'];
+		$name=$p['name'];
+		$vorname=$p['vorname'];
+		$data=DB::get_assoc_row("SELECT sem FROM ".$tpref."waehlt WHERE sem IN (".$semno[0].",".$semno[1].") AND snr='$snr' AND fachkurz='$fkurz' ORDER BY sem");
 		echo $snr.','.$name.','.$vorname.',';
 		if ($data['sem']==$semno[0]) echo $fkurz.',_'; else echo '_,'.$fkurz;
 		echo "\n";
 	}
-	
-	/*
-	SELECT sema.snr, sema.fachkurz AS sem1, semb.fachkurz AS sem2, sema.sem as x1, semb.sem as x2
-
-FROM waehlt AS sema
-
-LEFT JOIN waehlt AS semb
-
-ON (sema.snr = semb.snr)
-
-WHERE sema.sem = 1 AND semb.sem = 2
-
-	*/
 	
 ?>
